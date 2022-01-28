422cd1897b09a49e0776ba86b2f0dc0e8042eaa2
bymagka <bymagka@bk.ru>
2022-01-28 22:32:14 +0300

    базовый репозиторий

---
+++ Robot.Domain.Context/Repos/Interfaces/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Robot.DAL.Repos.Interfaces
+{
+    public interface IRepo<T>
+    {
+        int Add(T entity);
+
+        T Get(int id);
+
+        int Save(T entity);
+
+        bool Delete(T entity);
+
+        List<T> GetAll();
+    }
+}
