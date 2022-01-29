71b1f1265f12497c429e44f5369be495faa21e94
bymagka <bymagka@bk.ru>
2022-01-29 09:59:59 +0300

    комментарии

--- Robot.Domain.Context/Repos/Interfaces/IRepo.cs
+++ Robot.Domain.Context/Repos/Interfaces/IRepo.cs
@@ -4,14 +4,38 @@ namespace Robot.DAL.Repos.Interfaces
 {
     public interface IRepo<T>
     {
+        /// <summary>
+        /// Добавляет в бд новый объект
+        /// </summary>
+        /// <param name="entity">Новый объект</param>
+        /// <returns>Идентификатор сохраненного объекта</returns>
         int Add(T entity);
 
+        /// <summary>
+        /// Получает запись бд по идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор искомого объекта</param>
+        /// <returns>Найденная сущность</returns>
         T Get(int id);
 
+        /// <summary>
+        /// Сохраняет изменения в объекте бд
+        /// </summary>
+        /// <param name="entity">Сущность, по которой произошли изменения</param>
+        /// <returns>Идентификатор сохраняемой сущности</returns>
         int Save(T entity);
 
+        /// <summary>
+        /// Удаляет запись базы данных
+        /// </summary>
+        /// <param name="entity">Сущность, которую надо удалить</param>
+        /// <returns>Флаг успешности удаления</returns>
         bool Delete(T entity);
 
+        /// <summary>
+        /// Получает все записи таблицы типа
+        /// </summary>
+        /// <returns></returns>
         List<T> GetAll();
     }
 }
