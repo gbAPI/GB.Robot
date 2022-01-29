6bfd7955aaf737b1fa5f5569823ca35ca5ca066a
bymagka <bymagka@bk.ru>
2022-01-29 09:45:45 +0300

    Репозиторий для внешних объектов

---
+++ Robot.Domain.Context/Repos/ExternalObjectsRepo.cs
@@ -0,0 +1,34 @@
+﻿using Robot.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Robot.DAL.Repos
+{
+    public class ExternalObjectsRepo : IDisposable
+    {
+        private readonly RobotContext _dbContext;
+
+        public ExternalObjectsRepo()
+        {
+            _dbContext = new RobotContext();
+        }
+
+        public List<ScannerField> GetAllScannerFields()
+        {
+            return _dbContext.Set<ScannerField>().ToList();
+        }
+
+        public List<Template> GetAllTemplates()
+        {
+            return _dbContext.Set<Template>().ToList();
+        }
+
+        public void Dispose()
+        {
+            _dbContext?.Dispose();
+        }
+    }
+        
+    
+}
