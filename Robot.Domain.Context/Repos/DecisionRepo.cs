26a28710dac69e574db000ddedc1441c8acea397
bymagka <bymagka@bk.ru>
2022-01-29 09:56:02 +0300

    исправление проблемы с подчиненными записями

--- Robot.Domain.Context/Repos/DecisionRepo.cs
+++ Robot.Domain.Context/Repos/DecisionRepo.cs
@@ -1,9 +1,15 @@
-﻿using Robot.DAL.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Robot.DAL.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Robot.DAL.Repos
 {
     public class DecisionRepo : BaseRepo<Decision>
     {
-        
+        public override List<Decision> GetAll()
+        {
+            return _table.Include(x => x.RequiredFields).ToList();
+        }
     }
 }
