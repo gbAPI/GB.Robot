59a1c125ed04fd1ad8eca140b45e1c8ed0952548
bymagka <bymagka@bk.ru>
2022-02-05 23:00:52 +0300

    комментарии

--- Robot.Core.BusinessObjects/Models/Integration/Interfaces/IMessageData.cs
+++ Robot.Core.BusinessObjects/Models/Integration/Interfaces/IMessageData.cs
@@ -4,10 +4,19 @@ using System.Collections.Generic;
 
 namespace Robot.Core.Integration
 {
+    /// <summary>
+    /// Описание структуры входящего сообщения
+    /// </summary>
     public interface IMessageData
     {
+        /// <summary>
+        /// Тип отсканированного документа
+        /// </summary>
         string DocumentType { get; set; }
 
+        /// <summary>
+        /// Метаданные отсканированного документа.
+        /// </summary>
         Dictionary<string, IEnumerable<string>> InputData { get; set; }
     }
 }
--- Robot.Core.Services/Interfaces/IProcessingService.cs
+++ Robot.Core.Services/Interfaces/IProcessingService.cs
@@ -2,8 +2,17 @@
 
 namespace Robot.Core
 {
+    /// <summary>
+    /// Сервис, отвечающий за обработку входящего сообщения 
+    /// </summary>
     public interface IProcessingService
     {
+        /// <summary>
+        /// Обработчик входящего сообщения отсканированного документа
+        /// </summary>
+        /// <param name="inputData">Входящее сообщение</param>
+        /// <param name="templateId">Идентификатор результирующего шаблона. При неуспешности обработки = -1</param>
+        /// <returns>Признак успешности обработки</returns>
         bool ProcessInputData(IMessageData inputData, out int templateId);
     }
 }
